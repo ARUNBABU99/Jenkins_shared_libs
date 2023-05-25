211eb8497d92b2a404cce2159c87daf15f016c3e
arun <arunbabu.it01@gmail.com>
2023-05-25 06:07:31 +0000

    test

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,6 +1,3 @@
-def docker (name) {
-    sh 'docker tag ${name} arunbabu01/${name}'
-    // sh 'docker login -u $dockerhub_USR -p $dockerhub_PSW'
-    // sh 'docker push arunbabu01/${name}'
+def call(name){
+    echo "hey ${name}"
 }
-
