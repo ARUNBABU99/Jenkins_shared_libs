b48f89558e1323db9c799efa65ecb5d65504128f
arun <arunbabu.it01@gmail.com>
2023-05-25 05:34:59 +0000

    password and user added as plain text

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,6 +1,6 @@
 def docker (name) {
     sh 'docker tag ${name} arunbabu01/${name}'
-    sh 'docker login -u $dockerhub_USR -p $dockerhub_PSW'
+    sh 'docker login -u arunbabu01 -p Arunappu@123#'
     sh 'docker push arunbabu01/${name}'
 }
 
