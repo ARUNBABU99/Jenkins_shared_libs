1194522a88fabbc5cf6fbcc4b976ffbcb8319f4f
ARUNBABU99 <91900166+ARUNBABU99@users.noreply.github.com>
2023-05-25 12:30:39 +0530

    Update login.groovy

--- vars/login.groovy
+++ vars/login.groovy
@@ -10,6 +10,6 @@ def call(name){
     echo "the image name willbe  arunbabu01/${name}"
 
     sh "docker tag ${name} arunbabu01/${name}"
-    sh "docker login -u ${username} -p ${password}
+    sh "docker login -u $username -p $password
     sh "docker push arunbabu01/${name}"
 }
